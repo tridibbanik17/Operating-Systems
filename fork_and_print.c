54595251099b7924a0a40afe0e6259e228fa55e7
Tridib Banik <tridib.perfect@gmail.com>
2026-02-06 22:19:29 -0500

    Replace putc with printf for printing 'Hello'
    Changed putc to printf for better output formatting.

--- fork_and_print.c
+++ fork_and_print.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
 int main() {
   // Since there are 3 forks in total, there will be 2^3 = 8 processes created. 
   // 8 processes will print "Hello" 8 times. 
   fork();
   fork();
   fork();
-  putc("Hello\n");
+  
+  printf("Hello\n"); // All 8 processes execute this
+    return 0;
 }
